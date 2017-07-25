07b3e3d14fcc60ad10fc536d6fe39bce5a77dfda
Rafal Wilinski <raf.wilinski@gmail.com>
2017-07-25 23:45:17 +0200

    Add getAuth tests

---
+++ src/__tests__/utils.test.js
@@ -0,0 +1,18 @@
+const { getAuth } = require('../utils');
+
+describe('getAuth(joinChar = \'?\')', () => {
+  test('returns empty string if env variable is not set', () => {
+    expect(getAuth()).toBe('');
+  });
+
+  test('returns query with default joinChar', () => {
+    process.env.GITHUB_ACCESS_TOKEN = 'abc';
+    expect(getAuth()).toBe('?access_token=abc');
+  });
+
+  test('respects custom joinchar', () => {
+    process.env.GITHUB_ACCESS_TOKEN = 'abc';
+    expect(getAuth('&')).toBe('&access_token=abc');
+  });
+});
+
